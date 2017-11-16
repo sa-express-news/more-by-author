2b065539fc4ecc79517cc072f73454f76957ec90
Kia Farhang <kfarhang0@gmail.com>
2017-11-16 16:16:02 -0600

    Add file to hold collection of author:feed mappings

---
+++ src/authorFeedMap.ts
@@ -0,0 +1,10 @@
+import { AuthorFeedMap } from './interfaces';
+
+const authorFeeds: AuthorFeedMap[] = [
+    {
+        name: 'Rye Druzin',
+        feedURL: '/news/feed/Articles-by-Rye-Druzin-1908.php'
+    }
+]
+
+export default authorFeeds;
